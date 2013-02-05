46a0b80dace52f2649bad512c7ab8a6987aabf58
a_x <a-x@ukr.net>
2013-02-05 14:02:00 -0500

    - ax.js

--- ax.js
+++ ax.js
@@ -7,6 +7,6 @@ console.log("In ax", ax, ax.jquery)
 
 ax.util = require("./util");
 ax.widget = require("./widget");
-ax.widgetTemplate = require("./widget-temlate");
+ax.widgetTemplate = require("./widget-template");
 ax.servicerpc = require("./servicerpc");
 ax.loader = require("./loader");
