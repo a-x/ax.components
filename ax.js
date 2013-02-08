0948451f0795385b863d9b1ea0e60308d7e2b4be
a_x <a-x@ukr.net>
2013-02-08 10:52:27 -0500

    -service rpc

--- ax.js
+++ ax.js
@@ -1,12 +1,9 @@
 var ax = module.exports = {};
 
 //ax.jquery = require("jquery");
-require("sockjs");
-
 console.log("In ax", ax, ax.jquery)
 
 ax.util = require("./util");
 ax.widget = require("./widget");
 ax.widgetTemplate = require("./widget-template");
-ax.servicerpc = require("./servicerpc");
 ax.loader = require("./loader");
